7abe5109fe117de9b4eab37fad0b5fbb9e75caf3
Ashrit Verma <ashrit.verma@duke.edu>
2020-11-02 16:59:34 -0500

    Added PercolationBFS.java extending PercolationDFSFast.java

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -1,2 +1,11 @@
-public class PercolationBFS {
+import java.util.Queue;
+
+public class PercolationBFS extends PercolationDFSFast{
+    public PercolationBFS(int size){
+        super(size);
+    }
+    @Override
+    public void updateOnOpen(int row, int col) {
+        Queue<int[]>;
+    }
 }
